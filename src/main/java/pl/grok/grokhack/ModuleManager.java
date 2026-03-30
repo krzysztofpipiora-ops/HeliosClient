ce4a77f2ab378d625cbea1bafa595acea83c32e5
krzysztofpipiora-ops <krzysztof.pipiora@gmail.com>
2026-03-30 13:26:41 +0200

    Create ModuleManager.java

---
+++ src/main/java/pl/grok/grokhack/ModuleManager.java
@@ -0,0 +1,12 @@
+package pl.grok.grokhack;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ModuleManager {
+    public List<Module> modules = new ArrayList<>();
+
+    public ModuleManager() {
+        System.out.println("[GrokHack] ModuleManager załadowany - dodaj moduły w następnych krokach.");
+    }
+}
